544777885be9c42c72fb139d88ebeadd2ed47dae
Sergey <92623209+S3r4h@users.noreply.github.com>
2021-10-23 11:18:43 +0300

    Add files via upload
    Через цикл For

---
+++ 2 задача/2 задача/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _2_задача
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("p = ");
+            int p = Convert.ToInt32(Console.ReadLine());
+            int sum = 1;
+            for (int i = 1; i <= p; i++)
+            {
+                sum *= i;
+            }
+            Console.WriteLine("Сумма факториала {0} равна {1}", p, sum);
+        }
+    }
+}
